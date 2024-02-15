bd359672e0c719c10b7fae4d3667a9fd0aada953
Dinesh <dineshnani1443@gmail.com>
2024-02-15 11:45:42 +0530

    first commit

---
+++ src/test/java/TestCases/Login_Testcase.java
@@ -0,0 +1,31 @@
+package TestCases;
+
+import org.testng.annotations.Test;
+
+import pageObjects.Homepage;
+import pageObjects.LoginPage;
+import testBase.Baseclass;
+
+public class Login_Testcase extends Baseclass{
+	@Test(groups= {"regression","master"})
+	public void login() {
+		
+		Homepage hp=new Homepage(driver);
+		hp.ClickMyAccount();
+		hp.clickLogin();
+		
+		
+		
+		LoginPage lp=new LoginPage(driver);
+		lp.sendEmail(rb.getString("email"));
+		lp.sendPassword(rb.getString("password"));
+		lp.clickOnLogin();
+		
+		
+		
+		
+	}
+	
+	
+
+}
